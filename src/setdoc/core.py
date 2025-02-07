c5896cb6034da2e7aeb5c5092d62b68212531ff8
Johannes <johannes-programming@mailfence.com>
2025-02-07 10:40:42 +0100

    Version 0.1.3+local.5

--- src/setdoc/core.py
+++ src/setdoc/core.py
@@ -6,11 +6,11 @@ __all__ = ["setdoc"]
 
 @dataclasses.dataclass
 class setdoc:
-    "A class to set doc strings."
+    "This class helps to set doc strings."
 
     doc: Any
 
     def __call__(self, target: Any) -> Any:
-        "Set the doc string of the passed target " "to the value stored in the doc field " "of the setdoc object."
+        "This magic method implements calling the current instance. It sets the doc string of the passed target to the value stored in the doc field of the setdoc object."
         target.__doc__ = self.doc
         return target

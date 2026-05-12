0cff2eb7b76a77fcb56e29f1fa0853ee45c631b2
Johannes <johannes-programming@mailfence.com>
2026-05-12 16:19:40 +0200

    Version 1.3.8

--- src/setdoc/__init__.py
+++ src/setdoc/__init__.py
@@ -1,1 +1,7 @@
-from setdoc.core import *
+from setdoc.core.basic import basic
+from setdoc.core.getbasicdoc import getbasicdoc
+from setdoc.core.SetDoc import SetDoc
+
+__all__ = ["SetDoc", "basic", "getbasicdoc", "setdoc"]
+
+setdoc: type[SetDoc] = SetDoc  # legacy
--- src/setdoc/core/getbasicdoc.py
+++ src/setdoc/core/getbasicdoc.py
@@ -1,10 +1,10 @@
-from typing import *
+from typing import Optional, cast
 
 from setdoc._utils import Cfg
 
 __all__ = ["getbasicdoc"]
 
 
-def getbasicdoc(name: str) -> str:
+def getbasicdoc(name: str) -> Optional[str]:
     "This function returns the basic docstring for a given name."
-    return Cfg.cfg.data["basic"][name]
+    return cast(Optional[str], Cfg.cfg.basic.get(name))

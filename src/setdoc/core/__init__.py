8caf8eebfd91d41fcbec8585a0f9e6135bdc0dcc
Johannes <johannes-programming@mailfence.com>
2026-05-12 16:12:39 +0200

    Version 1.2.12

--- src/setdoc/core/__init__.py
+++ src/setdoc/core/__init__.py
@@ -1,45 +1,7 @@
-import dataclasses
-import enum
-import functools
-import tomllib
-from importlib import resources
-from typing import *
+from setdoc.core.basic import basic
+from setdoc.core.getbasicdoc import getbasicdoc
+from setdoc.core.SetDoc import SetDoc
 
 __all__ = ["SetDoc", "basic", "getbasicdoc", "setdoc"]
 
-
-class Util(enum.Enum):
-    util = None
-
-    @functools.cached_property
-    def data(self: Self) -> dict:
-        "This cached property holds the cfg data."
-        text: str
-        text = resources.read_text("setdoc.core", "cfg.toml")
-        return tomllib.loads(text)
-
-
-@dataclasses.dataclass
-class SetDoc:
-    "This class helps to set doc strings."
-
-    doc: Any
-
-    def __call__(self: Self, target: Any) -> Any:
-        "This magic method implements calling the current instance. It sets the doc string of the passed target to the value stored in the doc field of the setdoc object."
-        target.__doc__ = self.doc
-        return target
-
-
 setdoc = SetDoc  # legacy
-
-
-def basic(value: Any) -> Any:
-    "This decorator sets the docstring of the given value to what is suggested by its name."
-    value.__doc__ = getbasicdoc(value.__name__)
-    return value
-
-
-def getbasicdoc(name: str) -> str:
-    "This function returns the basic docstring for a given name."
-    return Util.util.data["basic"][name]

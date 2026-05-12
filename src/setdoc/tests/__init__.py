f42c97c04301e90657bd60ea369dbfa3ab9aac48
Johannes <johannes-programming@mailfence.com>
2026-05-12 16:10:50 +0200

    Version 1.2.9

--- src/setdoc/tests/__init__.py
+++ src/setdoc/tests/__init__.py
@@ -4,8 +4,8 @@ __all__ = ["test"]
 
 
 def test() -> unittest.TextTestResult:
-    loader: unittest.TestLoader = unittest.TestLoader()
-    tests: unittest.TestSuite = loader.discover(start_dir="setdoc.tests")
-    runner: unittest.TextTestRunner = unittest.TextTestRunner()
-    result: unittest.TextTestResult = runner.run(tests)
-    return result
+    loader: unittest.TestLoader
+    suite: unittest.TestSuite
+    loader = unittest.TestLoader()
+    suite = loader.discover(start_dir="setdoc.tests")
+    return unittest.TextTestRunner().run(suite)
